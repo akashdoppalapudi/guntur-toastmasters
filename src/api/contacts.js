748fb9cf6f530b7709be38d4f51d973b161f50dc
Akash Doppalapudi <akashdoppalapudi2001@gmail.com>
2021-07-18 13:22:31 +0530

    Added headers to requests

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -2,11 +2,20 @@ import axios from "axios";
 
 const url = "http://guntur-toastmasters-api.herokuapp.com/contacts";
 
-export const fetchContacts = () => axios.get(url);
+export const fetchContacts = () =>
+	axios.get(url, { headers: { "Access-Control-Allow-Origin": "*" } });
 
-export const createContact = (newContact) => axios.post(url, newContact);
+export const createContact = (newContact) =>
+	axios.post(url, newContact, {
+		headers: { "Access-Control-Allow-Origin": "*" },
+	});
 
 export const updateContact = (id, updatedContact) =>
-	axios.patch(`${url}/${id}`, updatedContact);
+	axios.patch(`${url}/${id}`, updatedContact, {
+		headers: { "Access-Control-Allow-Origin": "*" },
+	});
 
-export const deleteContact = (id) => axios.delete(`${url}/${id}`);
+export const deleteContact = (id) =>
+	axios.delete(`${url}/${id}`, {
+		headers: { "Access-Control-Allow-Origin": "*" },
+	});
--- src/api/upcomings.js
+++ src/api/upcomings.js
@@ -2,11 +2,20 @@ import axios from "axios";
 
 const url = "http://guntur-toastmasters-api.herokuapp.com/upcomings";
 
-export const fetchUpcomings = () => axios.get(url);
+export const fetchUpcomings = () =>
+	axios.get(url, { headers: { "Access-Control-Allow-Origin": "*" } });
 
-export const createUpcoming = (newUpcoming) => axios.post(url, newUpcoming);
+export const createUpcoming = (newUpcoming) =>
+	axios.post(url, newUpcoming, {
+		headers: { "Access-Control-Allow-Origin": "*" },
+	});
 
 export const updateUpcoming = (id, updatedUpcoming) =>
-	axios.patch(`${url}/${id}`, updatedUpcoming);
+	axios.patch(`${url}/${id}`, updatedUpcoming, {
+		headers: { "Access-Control-Allow-Origin": "*" },
+	});
 
-export const deleteUpcoming = (id) => axios.delete(`${url}/${id}`);
+export const deleteUpcoming = (id) =>
+	axios.delete(`${url}/${id}`, {
+		headers: { "Access-Control-Allow-Origin": "*" },
+	});
